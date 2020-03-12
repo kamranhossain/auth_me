9cfd7e1abd39a608662b11b7262e339e3880d9a1
kamranhossain <kamran.hossain227@gmail.com>
2020-03-13 05:26:45 +0600

    add password hashing functionality

--- lib/auth_me/user_manager/user.ex
+++ lib/auth_me/user_manager/user.ex
@@ -1,6 +1,7 @@
 defmodule AuthMe.UserManager.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Argon2
 
   schema "users" do
     field :password, :string
@@ -14,5 +15,12 @@ defmodule AuthMe.UserManager.User do
     user
     |> cast(attrs, [:username, :password])
     |> validate_required([:username, :password])
+    |> put_password_hash()
   end
+  
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Argon2.hash_pwd_salt(password))
+  end
+  
+  defp put_password_hash(changeset), do: changeset
 end
