5038d4fbfec8494f9ba518b5e67e90eacdab658e
kamranhossain <kamran.hossain227@gmail.com>
2020-03-13 05:58:31 +0600

    wire up guardian auth with router

--- lib/auth_me_web/router.ex
+++ lib/auth_me_web/router.ex
@@ -13,10 +13,39 @@ defmodule AuthMeWeb.Router do
     plug :accepts, ["json"]
   end
 
+  # Our pipeline implements "maybe" authenticated. We'll use the `:ensure_auth` below for when we need to make sure someone is logged in.
+  pipeline :auth do
+    plug AuthMe.UserManager.Pipeline
+  end
+
+  # We use ensure_auth to fail if there is no one logged in
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
   scope "/", AuthMeWeb do
     pipe_through :browser
 
+    # get "/", PageController, :index
+  end
+
+
+    # Maybe logged in routes
+  scope "/", AuthMeWeb do
+    pipe_through [:browser, :auth]
+
     get "/", PageController, :index
+
+    get "/login", SessionController, :new
+    post "/login", SessionController, :login
+    get "/logout", SessionController, :logout
+  end
+
+  # Definitely logged in scope
+  scope "/", AuthMeWeb do
+    pipe_through [:browser, :auth, :ensure_auth]
+
+    get "/protected", PageController, :protected
   end
 
   # Other scopes may use custom stacks.
