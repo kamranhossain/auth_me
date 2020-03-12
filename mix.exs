a074c3ec048c4b15686c3a47ddfc11752f283e19
kamranhossain <kamran.hossain227@gmail.com>
2020-03-13 04:36:35 +0600

    add guardian and argon2 elixir package

--- mix.exs
+++ mix.exs
@@ -43,6 +43,8 @@ defmodule AuthMe.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:guardian, "~> 2.1"},
+      {:argon2_elixir, "~> 2.0"}
     ]
   end
 
