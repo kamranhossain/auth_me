3a4c538e1b402574f5b81ff87d98af5a5cc04566
kamranhossain <kamran.hossain227@gmail.com>
2020-03-13 04:48:34 +0600

    add guardian generated secret to guardian config

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Configures Guardian Authentication
+config :auth_me, AuthMe.UserManager.Guardian,
+  issuer: "auth_me",
+  secret_key: "rDVIL4pp9Hx1FPIa4pQRY+1Lih8ETh3mYNDY/HfejU+cE5WPPT4UeiEyNFyV9HNy"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
